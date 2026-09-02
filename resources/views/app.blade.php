e30582693e303acd52b9c8bc0d8f953ec503adcb
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-02 13:38:43 +0100

    feat: add styled E favicon and link tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Event Ticketing</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <link rel="apple-touch-icon" href="/favicon.png">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @routes
     @inertiaHead
